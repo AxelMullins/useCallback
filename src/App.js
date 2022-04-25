1b7c3aca8a14ad15aba7869c38ad8c506b1d13b2
Axel Mullins <axelmullins94@gmail.com>
2022-04-25 09:09:51 -0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState(0)
+  const [num1] = useState(2);
+  const [num2] = useState(2);
+
+  // use useCallback if we have our function in dependency array on useEfect.
+  const suma = useCallback(() => num1 + num2, [num1, num2]);
+  const buildArray = useCallback(() => [num1, num2], [num1, num2])
+
+  useEffect(() => {
+    console.log(`You use ${suma()}`);
+    setResult(suma())
+  }, [suma]);
+
+  useEffect(() => {
+    console.log(`You use ${buildArray()}`);
+    setResult(buildArray())
+  }, [buildArray]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <label htmlFor="search">Search: </label>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <p hidden>{result}</p>
+    </main>
   );
 }
 
